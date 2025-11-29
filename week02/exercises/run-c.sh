b78658dff2825e6749a8c832b2616de8cd0cd1fe
Nkululeko Jonas <3524042+nkululekojonas@users.noreply.github.com>
2025-11-29 21:49:17 +1000

    feat: added flags to run-c script

--- week02/exercises/run-c.sh
+++ week02/exercises/run-c.sh
@@ -2,53 +2,121 @@
 #
 # run-c.sh: Compile and run C programs, logging errors to file.
 
-if [[ $# -eq 0 ]]
-then
+usage()
+{
     echo "Usage: $0 file.c [args...]"
     exit 1
+}
+
+all=false
+clean=false
+debug=false
+watch=false
+verbose=false
+
+parse_flags()
+{
+    for flag in "$@"
+    do
+        case "$flag" in
+            -c | --clean)
+                clean=true
+                ;;
+            -v | --verbose)
+                verbose=true
+                ;;
+            --debug)
+                debug=true
+                ;;
+            --watch)
+                watch=true
+                ;;
+            *)
+                usage
+                ;;
+        esac
+    done
+}
+
+if [[ $# -eq 0 ]]
+then
+    usage
 fi
+
+# Initialise arrays
+flags=()
+sources=()
 
 while [[ $# -gt 0 ]]
 do
     case "$1" in
-        '-c' | '--clean')
-            clean=true
+        --)
+            shift
+            break
+            ;;
+        -*)
+            flags+=("$1")
             shift
             ;;
-        '-v' |'--verbose')
-            verbose=true
+        *.c)
+            sources+=("$1")
             shift
             ;;
-        *) break
+        *) 
+            usage
             ;;
     esac
 done
 
-FILE="$1"
-shift
+args=("$@")
+parse_flags "${flags[@]}"
 
-if [[ ! -e "$FILE" ]]
-then
-    echo "$0: $FILE: No such file"
-    exit 1
-fi 
+validate_file()
+{
+    for file in "$@"
+    do
+        if [[ ! -e "$file" ]]
+        then
+            echo "$0: $file: No such file"
+            exit 1
+        fi 
 
-if [[ ! "$FILE" == *.c ]]
-then
-    echo "$FILE: Not valid c file"
-    exit 1
-fi
+        if [[ ! "$file" == *.c ]]
+        then
+            echo "$file: Not valid c file"
+            exit 1
+        fi
+    done
+}
+
+validate_file "${sources[@]}"
 
 ERROR_FILE="errors.log"
-EXECUTABLE="${FILE/.c/''}"
+EXECUTABLE="build"
+COMM_ARGS=("${sources[@]}" "-Wall" "-Werror" "-Wextra" "-o" "${EXECUTABLE}")
 
-if gcc "$FILE" -Wall -Werror -Wextra -o "$EXECUTABLE" 2> "$ERROR_FILE"
+if [[ "$debug" = true ]]
 then
-    "./$EXECUTABLE" "$@" 
+    COMM_ARGS+=("-g")
+fi
+
+if [[ "$verbose" = true ]]
+then
+    echo "[verbose] gcc ${COMM_ARGS[@]}"
+fi
+    
+if gcc "${COMM_ARGS[@]}" 2> "$ERROR_FILE"
+then
+
+    if [[ "$verbose" = true ]]
+    then
+        echo "[verbose] ./${EXECUTABLE} ${args[@]}"
+    fi
+    "./$EXECUTABLE" "${args[@]}"
 
     if [[ "$clean" = true ]]
     then
-        rm "$ERROR_FILE" "$EXECUTABLE"
+        rm "$ERROR_FILE"
     fi
 else
     echo "Compilation failed. See ${ERROR_FILE}."
